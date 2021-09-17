7bae3931c7839fad48aa36e778667bdbf5f561cc
Michael Watson <michaelwtsn4@gmail.com>
2021-09-17 12:56:23 -0400

    Add configs for prod

--- src/flask_config.py
+++ src/flask_config.py
@@ -11,9 +11,15 @@ class Config(object):
 class Production(Config):
     DEVELOPMENT = False
     DEBUG = False
-    DB_HOST = 'my.production.database'
+    DB_HOST = 'chainedmetrics-prod-do-user-9754357-0.b.db.ondigitalocean.com'
+    DB_USER = 'flask_app'
+    DB_PORT = 25060
+    DATABASE = 'metrics'
+    DB_PASS = os.environ['DB_PASS']
     JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
-    JWT_TOKEN_LOCATION = 'headers'
+    JWT_TOKEN_LOCATION = "headers"
+
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DATABASE}'
 
 class Development(Config):
     DEVELOPMENT = True
