8596bdc1e3c518b93142ec3fe03f8a76f33ca055
Michael Watson <michaelwtsn4@gmail.com>
2021-09-24 12:35:06 -0400

    Update local scripts for the gmail config

--- scripts/local_server.sh
+++ scripts/local_server.sh
@@ -1,7 +1,7 @@
 docker run \
 -a stdout -a stderr -p 5050:5050 -it \
 -e DB_PASS=$DEV_DB_PASS -e JWT_SECRET_KEY=stay_safe_out_there -e MAILCHIMP_API_KEY=$MAILCHIMP_API_KEY\
- -v ~/chained_metrics_api:/src/chainedmetrics_api \
+ -e GMAIL_PASS=$MAILCHIMP_API_KEY -v ~/chained_metrics_api:/src/chainedmetrics_api \
  -w /src/chainedmetrics_api/src \
  --entrypoint /opt/conda/bin/conda \
  registry.digitalocean.com/chainedmetrics/chainedmetrics_api:$1 \
