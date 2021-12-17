4f14b4ac2fffbac83e2cc8aae19a44f151dead7c
Michael Watson <michaelwtsn4@gmail.com>
2021-12-17 15:40:41 -0500

    Update environment variables to point to dev

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,7 +7,7 @@ docker build -t test:$tag .
 docker run \
 -a stdout -a stderr -p 5050:5050 -it \
 -e DB_PASS=$DEV_DB_PASS -e JWT_SECRET_KEY=stay_safe_out_there -e MAILCHIMP_API_KEY=$MAILCHIMP_API_KEY\
- -e GMAIL_PASS=$MAILCHIMP_API_KEY -v ~/chained_metrics_api:/src/chainedmetrics_api \
+ -e GMAIL_PASS=$MAILCHIMP_API_KEY -e TEST=true -v ~/chained_metrics_api:/src/chainedmetrics_api \
  -w /src/chainedmetrics_api/src \
  --entrypoint /opt/conda/bin/conda \
  test:$tag \
