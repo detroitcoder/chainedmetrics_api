c6bcbe050ecd3bc738b33f4521a37a6b1c530c4f
Michael Watson <michaelwtsn4@gmail.com>
2021-12-26 17:03:20 -0500

    Rename thel local bash script

--- scripts/run_local_bash.sh
+++ scripts/run_local_bash.sh
@@ -1,5 +1,5 @@
 docker run \
--p 5050:5050 -it \
+-it \
 -e CHAINEDMETRICS_ENV=Development \
 -e DB_PASS=$DEV_DB_PASS -e JWT_SECRET_KEY=stay_safe_out_there -e MAILCHIMP_API_KEY=$MAILCHIMP_API_KEY\
  -e GMAIL_PASS=$MAILCHIMP_API_KEY -v ~/chained_metrics_api:/src/chainedmetrics_api \
